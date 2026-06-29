f7dff1b28d7fde6fe8035c771672b5489d14e3be
Mariano Alvizouri <ealvizouri@gmail.com>
2026-06-28 21:10:14 -0600

    feat(intro): add Intro section with fullscreen image

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import Countdown from '@/components/Countdown'
 import Details from '@/components/Details'
 import DressCode from '@/components/DressCode'
 import Hero from '@/components/Hero'
+import Intro from '@/components/Intro'
 import OurStory from '@/components/OurStory'
 import Programa from '@/components/Programa'
 import Rsvp from '@/components/Rsvp'
@@ -16,6 +17,7 @@ export default function Page() {
   return (
     <div>
       <Hero />
+      <Intro />
       <Countdown />
       <OurStory />
       <ChapterOne />
---
+++ components/Intro.tsx
@@ -0,0 +1,38 @@
+import Image from 'next/image'
+import DoubleChevronDown from './DoubleChevronDown'
+
+export default function Intro() {
+  return (
+    <section
+      id="story"
+      className="relative min-h-screen overflow-hidden px-6 py-20"
+    >
+      <Image
+        src="/assets/home/shared/background_crema.png"
+        alt=""
+        fill
+        className="object-cover"
+      />
+
+      <div className="relative z-10 mx-auto flex w-full max-w-sm flex-col items-center gap-12 md:max-w-5xl md:gap-16">
+        {/* Photo collage */}
+        <div className="relative w-full shrink-0 md:w-1/2">
+          <Image
+            src="/assets/home/Intro/intro.png"
+            alt=""
+            width={400}
+            height={400}
+            className="w-full!"
+          />
+        </div>
+
+        <div className="flex justify-center">
+          <DoubleChevronDown
+            hashtag="chapter-one"
+            className="position static text-[#670626]/75 hover:text-[#670626]"
+          />
+        </div>
+      </div>
+    </section>
+  )
+}
